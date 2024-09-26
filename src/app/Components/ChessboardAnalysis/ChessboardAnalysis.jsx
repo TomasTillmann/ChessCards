3605adbda3ad806435c74d526021b8e38750ba3a
tomas tillmann <tomas.tilllmann@gmail.com>
2024-09-26 11:20:17 +0200

    removed old analysis board implementation

--- src/app/Components/ChessboardAnalysis/ChessboardAnalysis.jsx
+++ src/app/Components/ChessboardAnalysis/ChessboardAnalysis.jsx
@@ -1,9 +1,5 @@
 'use client'
 
-import {Chessboard} from "react-chessboard";
-import {Chess} from "chess.js";
-import {useState} from "react";
-
 import ChessAnalysisBoard from 'react-chess-analysis-board'
 import './style.scss';
 
@@ -17,45 +13,4 @@ export const ChessboardAnalysis = ({ position }) => {
             }}
         />
     )
-}
-
-/// OLD ??
-export const ChessboardLegalOld = ({ position }) => {
-    const [fen, setFen] = useState(position);
-    const [game, setGame] = useState(new Chess(position));
-
-    function onDrop (source, target) {
-        const result = makeAMove({
-            from: source,
-            to: target,
-            promotion: "q", //TODO: always promote to a queen for example simplicity
-        });
-
-        // illegal move
-        if (result == null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    function makeAMove(move) {
-        let result;
-
-        try {
-            result = game.move(move);
-            setGame(game);
-            setFen(game.fen());
-        }
-        catch { /* supress error */ }
-
-        return result;
-    }
-
-    return (
-        <Chessboard
-            position={fen}
-            onPieceDrop={onDrop}
-        />
-    );
 }
